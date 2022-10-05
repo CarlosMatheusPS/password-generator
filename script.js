c3ba73c3bc7ef257523d5a74b0d60cb3284608dc
Carlos Matheus <97807823+CarlosMatheusPS@users.noreply.github.com>
2022-10-05 03:23:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const generatedPassword = resultEl.innerText
+    if (!generatedPassword | generatedPassword == 'check character limits') { return }
+    textarea.value = generatedPassword
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard')
+})
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasUpper = uppercaseEl.checked
+    const hasLower = lowercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+    resultEl.innerText = generatePassword(length, hasUpper, hasLower, hasNumber, hasSymbol)
+})
+function generatePassword(length, upper, lower, number, symbol) {
+
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+
+    if (typesCount === 0) {
+        return ''
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+    const password = generatedPassword.slice(0, length)
+
+    const finalPassword = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
+
+    console.log(typeof (finalPassword))
+
+    if (length <= 20 && length > 3) {
+        return finalPassword
+
+    } else {
+        return resultEl.innerText = 'check character limits'
+    }
+
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+function getRandomSymbol() {
+    const symbols = '!@#$%¨&*()-=[]{}<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
